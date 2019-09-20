80d88fa01477186bdde25a1d1dfcbfd8f15cecc9
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-09-20 22:23:12 +0900

    Add: wifi function

--- syateki_gun_main.ino
+++ syateki_gun_main.ino
@@ -3,6 +3,7 @@
  * @brief 射的銃メインプログラム
  */
 #include <Arduino.h>
+#include <WiFi.h>
 #include "irShooter.hpp"
 #include "reactor.hpp"
 
@@ -22,6 +23,19 @@ Reactor reactor;
 void setup(void) {
   Serial.begin(115200);
   initializePins();
+  WiFi.begin("your-SSID", "your-PASSWORD");
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  if (WiFi.status() == WL_CONNECTED) {  
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("WiFi connect process time out.");
+  }
 }
 
 /**
@@ -37,6 +51,7 @@ void loop(void) {
       //  チャタリング除去対応
       delay(100);
       if(digitalRead(PIN_IN_TRIGGER) == LOW){
+        get_root();
         ir_shooter.shoot();
         Serial.println("# shoot (remain bullets = " + String(ir_shooter.get_bullets_num()) + ")");
         reactor.react_to_fire(ir_shooter.get_bullets_num());
@@ -55,4 +70,32 @@ void loop(void) {
  */
 static void initializePins(){
   pinMode(PIN_IN_TRIGGER, INPUT);
+}
+
+static void get_root(){
+  const char* host      = "192.168.100.114";
+  
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  WiFiClient client;
+  const int Port = 5000;
+  if (!client.connect(host, Port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  String url = "/";
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+    "Host: " + host + "\r\n" + 
+    "Connection: close\r\n\r\n");
+  delay(1000);
+
+  // 返ってきた情報の解析
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    line.trim();
+    Serial.println(line);
+  }
+  Serial.println();
+  Serial.println("closing connection");
 }
