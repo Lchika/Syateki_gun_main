9701fc336fd030b8849d4fa01b6d350a85de84f8
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-09-22 21:56:31 +0900

    Add: debug print

--- syateki_gun_main.ino
+++ syateki_gun_main.ino
@@ -4,39 +4,53 @@
  */
 #include <Arduino.h>
 #include <WiFi.h>
+#include "debug.h"
 #include "irShooter.hpp"
 #include "reactor.hpp"
 
-#define PIN_IN_TRIGGER        14
-#define PIN_OUT_IR            13
-#define MAGAGINE_CAPACITY     20
-#define INITIAL_BULLETS_NUM   MAGAGINE_CAPACITY
-#define DELAY_WAIT_RESET_MS   100
-#define TOTAL_RESET_TIME_LIMIT 3000
+//  内部関数宣言
+void setup(void);
+void loop(void);
+static void initializePins(void);
+static int get_root(void);
+static int shoot(void);
+static void hit(void);
+static void wait_until(int total_time_ms, int ref_pin, int delay_wait_ms, uint8_t ref_level = LOW);
+static void reset_game(void);
 
+//  定数宣言
+static int constexpr PIN_IN_TRIGGER = 14;     //  トリガ入力ピン
+static int constexpr PIN_OUT_IR     = 13;     //  赤外線出力用ピン
+static unsigned int constexpr MAGAGINE_CAPACITY   = 20;                 //  マガジン最大装填量
+static unsigned int constexpr INITIAL_BULLETS_NUM = MAGAGINE_CAPACITY;  //  初期弾数
+static int constexpr DELAY_WAIT_RESET_MS    = 100;    //  リセット待機用delay時間[ms]
+static int constexpr TOTAL_RESET_TIME_LIMIT = 3000;   //  リセット待機用合計時間[ms]
+
+//  変数宣言
 IrShooter ir_shooter(PIN_OUT_IR, MAGAGINE_CAPACITY, INITIAL_BULLETS_NUM);
 Reactor reactor;
 
+//  関数定義
 /**
  * @brief セットアップ関数
  * @param None
  * @return None
  */
 void setup(void) {
-  Serial.begin(115200);
+  BeginDebugPrint();
   initializePins();
   WiFi.begin("ROBOCON-AP1", "20190216-rc");
   while(WiFi.status() != WL_CONNECTED){
     delay(500);
-    Serial.print(".");
+    DebugPrint(".");
   }
   if (WiFi.status() == WL_CONNECTED) {  
-    Serial.println("");
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    DebugPrint("");
+    DebugPrint("WiFi connected.");
+    DebugPrint("IP address: ");
+    DebugPrint(WiFi.localIP().toString().c_str());
   } else {
-    Serial.println("WiFi connect process time out.");
+    DebugPrint("WiFi connect process time out.");
   }
 }
 
@@ -62,7 +76,9 @@ void loop(void) {
     }
     //  弾を打ち切った場合
     if(ir_shooter.get_bullets_num() <= 0){
-      wait_reset();
+      wait_by_pinlevel(TOTAL_RESET_TIME_LIMIT, PIN_IN_TRIGGER, DELAY_WAIT_RESET_MS, LOW);
+      //  トリガを引いていた時間が閾値を超えたのでリセット
+      reset_game();
     }
     pre_is_trigger_on = true;
   }else{
@@ -75,7 +91,7 @@ void loop(void) {
  * @param None
  * @return None
  */
-static void initializePins(){
+static void initializePins(void){
   pinMode(PIN_IN_TRIGGER, INPUT);
 }
 
@@ -84,16 +100,16 @@ static void initializePins(){
  * @param None
  * @return None
  */
-static int get_root(){
+static int get_root(void){
   const char* host = "192.168.100.114";
   
-  Serial.print("connecting to ");
-  Serial.println(host);
+  DebugPrint("connecting to ");
+  DebugPrint(host);
   
   WiFiClient client;
   const int Port = 5000;
   if (!client.connect(host, Port)) {
-    Serial.println("connection failed");
+    DebugPrint("connection failed");
     return -1;
   }
   String url = "/";
@@ -108,7 +124,7 @@ static int get_root(){
   while(client.available()){
     String line = client.readStringUntil('\r');
     line.trim();
-    Serial.println(line);
+    DebugPrint(line.c_str());
     if(is_start_body){
       body += line;
     }
@@ -116,9 +132,10 @@ static int get_root(){
       is_start_body = true;
     }
   }
-  Serial.println();
-  Serial.println("body = " + body);
-  Serial.println("closing connection");
+  DebugPrint("");
+  DebugPrint("body =");
+  DebugPrint(body.c_str());
+  DebugPrint("closing connection");
   if(body == ""){
     return -1;
   }
@@ -130,13 +147,13 @@ static int get_root(){
  * @param None
  * @return None
  */
-static int shoot(){
-  //  センターに発射を通知し、的の判定を確認
+static int shoot(void){
+  //  センタサーバに発射を通知し、的の判定を確認
   int target_num = get_root();
-  Serial.println("target_num = " + String(target_num));
+  DebugPrint("target_num = %d", target_num);
   //  弾数管理を更新する
   ir_shooter.shoot();
-  Serial.println("# shoot (remain bullets = " + String(ir_shooter.get_bullets_num()) + ")");
+  DebugPrint("# shoot (remain bullets = %d)", ir_shooter.get_bullets_num());
   //  発射関係の演出を行う
   reactor.react_to_fire(ir_shooter.get_bullets_num());
   return target_num;
@@ -147,34 +164,45 @@ static int shoot(){
  * @param None
  * @return None
  */
-static void hit(){
+static void hit(void){
   //  得点間関係の演出を行う
   reactor.react_to_hit();
 }
 
 /**
- * @brief リセット待機
- * @param None
+ * @brief ピンレベルによる待機処理
+ * @param[in] total_time_ms   特定ピン状態合計時間[ms]
+ * @param[in] ref_pin         監視するピン番号
+ * @param[in] delay_wait_ms   ピンレベルを読み取る間隔[ms]
+ * @param[in] ref_level       HIGH or LOWどちらの状態の合計時間を記録するか
  * @return None
- * @note ゲーム開始時、ゲーム終了後等で使う
+ * @note  ref_pinの状態がref_levelである時間の合計値がtotal_time_msに達するまで待機する\n
+ *        ゲーム開始時、ゲーム終了後等で使う
  */
-static void wait_reset(){
+static void wait_by_pinlevel(int total_time_ms, int ref_pin, int delay_wait_ms, uint8_t ref_level){
   int total_push_time_ms = 0;
   bool is_pre_pushed = false;
   //  トリガを引いていた時間が閾値を超えるまでこのループにとどまり続ける
-  while(total_push_time_ms < TOTAL_RESET_TIME_LIMIT){
-    if(digitalRead(PIN_IN_TRIGGER) == LOW){
+  while(total_push_time_ms < total_time_ms){
+    if(digitalRead(ref_pin) == ref_level){
       if(is_pre_pushed){
-        total_push_time_ms += DELAY_WAIT_RESET_MS;
+        total_push_time_ms += delay_wait_ms;
       }else{
         is_pre_pushed = true;
       }
     }else{
       is_pre_pushed = false;
     }
-    delay(DELAY_WAIT_RESET_MS);
+    delay(delay_wait_ms);
   }
-  //  トリガを引いていた時間が閾値を超えたのでリセット
+}
+
+/**
+ * @brief ゲームリセット
+ * @param None
+ * @return None
+ */
+static void reset_game(void){
   ir_shooter.reload();
   reactor.reset();
 }
